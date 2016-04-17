312be4aa1b37cfce0d7fc5a29d2cc70e1b395b3d
Guilherme <guilherme.cmc@grad.ufsc.br>
2016-04-17 09:20:40 -0600

    JUnit tests

--- src/test/java/br/ufsc/labsec/desafio/TestLogin.java
+++ src/test/java/br/ufsc/labsec/desafio/TestLogin.java
@@ -1,8 +1,10 @@
 package br.ufsc.labsec.desafio;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.not;
 import org.junit.Test;
 
 /**
@@ -11,9 +13,20 @@ import org.junit.Test;
 public class TestLogin {
 	
 	@Test
-	public void testSanity() {
+	public void testSanity() throws Exception { 
 		Login login = new Login();
-		assertTrue(true);
-		assertEquals(login.getUser(), "");
+		login.setUser("Teste");
+		login.setPassword("senhamuitosegura");
+		login.register();
+		
+		assertEquals(login.getUser(), "Teste");
+		assertEquals(login.getPassword(),"senhamuitosegura");
+		assertThat(login.getUser(), not("NaoTeste"));
+		assertThat(login.getPassword(),not("senhanaotaosegura"));
+		assertTrue(login.check("senhamuitosegura", login.getSaltedHash(login.getPassword())));
+		assertFalse(login.check("senhanaotaosegura", login.getSaltedHash(login.getPassword())));
+		
+	
+		
 	}
 }
